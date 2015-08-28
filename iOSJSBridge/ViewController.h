bfa58edd56a19675db30d9ef2670dd9640242a50
niladrib <niladri.bora@gmail.com>
2015-08-28 10:30:08 -0700

    Initial Commit

---
+++ iOSJSBridge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSJSBridge
+//
+//  Created by Niladri Bora on 8/28/15.
+//  Copyright © 2015 LUMO Bodytech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSJSBridge/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSJSBridge
+//
+//  Created by Niladri Bora on 8/28/15.
+//  Copyright © 2015 LUMO Bodytech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
